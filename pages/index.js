c22424e16698dbcc4f9b077451e06e0c1ae6995c
Daniele <daniszcode>
2021-07-15 11:50:10 -0300

    Adicional a barra lateral do menu através da lib

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 //import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/components/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/components/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 //O nome da const/function precisa começar com letra maiuscula, pois se trata de um componente da pag
 //para usar "js + html" no react é necessario o '{{}}'
 
 function ProfileSidebar(props) {
   return (
+    <>
     <Box>
      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} ></img> 
+      <hr />  
+     <p> <a className="boxLink" href={`https://github.com/${props.githubUser}`} target="_blank" >  @{props.githubUser}</a></p>
+      <hr/>
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
+    </>
   )
 }
 
@@ -38,6 +44,18 @@ export default function Home() {
 
     <h1 className="title" >Bem-vindo(a)</h1>
     <OrkutNostalgicIconSet />
+
+  </Box>
+  <Box>
+    <h2>O que você deseja fazer?</h2>
+    <form>
+      <div>
+      <input placeholder="Qual vai ser o nome da sua comunidade?" 
+      name="title" 
+      aria-label="Qual vai ser o nome da sua comunidade?"
+      type="text" />
+      </div>
+    </form>
   </Box>
   </div>
   <div className="profileRelationArea" style={{ gridArea: 'profileRelationsArea' }}>
